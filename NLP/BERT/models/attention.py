d480ae35b308b8e429c70dd4b25d5bf8e9bb065f
Dante Lok <37895880+dantelok@users.noreply.github.com>
2025-02-28 18:16:09 +0800

    part of bert

--- NLP/BERT/models/attention.py
+++ NLP/BERT/models/attention.py
@@ -1,9 +1,51 @@
 import torch
 import torch.nn as nn
+from torch.nn.functional import F
 
-class Attention(nn.Module):
-    def __init__(self, d_model, num_heads=8, dropout=0.1):
+class ScaledDotProductAttention(nn.Module):
+    def __init__(self, dropout=0.1):
         super().__init__()
+        self.dropout = nn.Dropout(dropout)
 
-        self.num_heads = num_heads
+    def forward(self, q, k, v, mask=None):
+        d_k = q.shape[-1]
+
+        attention_score = torch.matmul(q, k.transpose(-1, -2)) / torch.sqrt(torch.tensor(d_k, dtype=torch.float32))
+
+        if mask is not None:
+            attention_score = attention_score.masked_fill(mask == 0, float('-inf'))
+
+        attention_weight = F.softmax(attention_score, dim=-1)
+        attention_weight = self.dropout(attention_weight)
+        attention_output = torch.matmul(attention_weight, v)
+
+        return attention_output, attention_weight
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model, num_heads, dropout=0.1):
+        super().__init__()
         self.d_model = d_model
+        self.num_heads = num_heads
+        self.d_k = d_model // num_heads
+
+        self.w_q = nn.Linear(d_model, d_model, bias=False)
+        self.w_k = nn.Linear(d_model, d_model, bias=False)
+        self.w_v = nn.Linear(d_model, d_model, bias=False)
+        self.w_o = nn.Linear(d_model, d_model, bias=False)
+
+        self.attention = ScaledDotProductAttention(dropout=dropout)
+        self.dropout = nn.Dropout(dropout)
+
+
+    def forward(self, q, k, v, mask=None):
+        batch_size = q.shape[0]
+
+        q = self.w_q(q)
+        k = self.w_k(k)
+        v = self.w_v(v)
+
+
+
+
+
